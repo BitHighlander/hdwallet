ae98bed1f7e3c9bba046fdc0afc1fb0e18fcea5e
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-10-31 08:41:16 +1100

    chore: wip

--- packages/hdwallet-walletconnectV2/src/adapter.ts
+++ packages/hdwallet-walletconnectV2/src/adapter.ts
@@ -1,6 +1,7 @@
 import { Events, Keyring } from "@shapeshiftoss/hdwallet-core";
 import { EthereumProvider } from "@walletconnect/ethereum-provider";
 import { EthereumProviderOptions } from "@walletconnect/ethereum-provider/dist/types/EthereumProvider";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5";
 
 import { WalletConnectV2HDWallet } from "./walletconnectV2";
 
@@ -27,11 +28,43 @@ export class WalletConnectV2Adapter {
         throw new Error("WalletConnectV2 provider configuration not set.");
       }
 
+      // 1. Get projectId
+      const projectId = "YOUR_PROJECT_ID";
+
+      // 2. Set chains
+      const mainnet = {
+        chainId: 1,
+        name: "Ethereum",
+        currency: "ETH",
+        explorerUrl: "https://etherscan.io",
+        rpcUrl: "https://cloudflare-eth.com",
+      };
+
+      // 3. Create modal
+      const metadata = {
+        name: "My Website",
+        description: "My Website description",
+        url: "https://mywebsite.com",
+        icons: ["https://avatars.mywebsite.com/"],
+      };
+
+      const modal = createWeb3Modal({
+        ethersConfig: defaultConfig({ metadata }),
+        chains: [mainnet],
+        projectId,
+      });
+
       const provider = await EthereumProvider.init(this.providerConfig);
+      const provider2 = await modal.getWalletProvider();
+
+      if (!provider2) {
+        throw new Error("WalletConnectV2: missing provider.");
+      }
+
       const wallet = new WalletConnectV2HDWallet(provider);
 
       //  Enable session (triggers QR Code modal)
-      await wallet.initialize();
+      // await wallet.initialize();
       const deviceID = await wallet.getDeviceID();
       this.keyring.add(wallet, deviceID);
       this.keyring.emit(["WalletConnectV2", deviceID, Events.CONNECT], deviceID);
